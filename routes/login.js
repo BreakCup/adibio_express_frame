eeb982a4cb0a8a2240006781c9debb5cfa25ebe5
breakcup <haoyuan_zhuhai@outlook.com>
2017-08-21 17:46:20 +0800

    修改login

--- routes/login.js
+++ routes/login.js
@@ -4,5 +4,8 @@ var login = express.Router();
 login.get('/', function(req, res) {
   res.render('login', { title: 'Express' });
 });
-
+login.post('/',function(req,res){
+	conselo.log("get post:\n");
+	conselo.log(req,res);
+});
 module.exports = login;
