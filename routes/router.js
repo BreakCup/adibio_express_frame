fc1c9c2fd9bbbd8219f2721f92026b6458739d7e
breakcup <haoyuan_zhuhai@outlook.com>
2017-08-21 09:59:16 +0800

    修改

--- routes/router.js
+++ routes/router.js
@@ -1,8 +1,8 @@
 
 module.exports = function(app){
-	app.use('/',require('/routes/login'));
-	app.use('/index', require('/routes/index'));
-	app.use('/users', require('/routes/users'));
+	app.use('/',require('./login'));
+	app.use('/index', require('./index'));
+	app.use('/users', require('./users'));
 }
 
 
